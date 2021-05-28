122b9d0a4dd8bd00a62f38ad205c1716a5271647
Manuel Perez <manuelpc2495@gmail.com>
2021-05-28 09:40:05 -0600

    change money admin site

--- money/admin.py
+++ money/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 
 from .models import Money, ExchangeRate
 
-admin.site.register(Money)
 admin.site.register(ExchangeRate)
+
+
+@admin.register(Money)
+class MoneyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'symbol', 'principal', 'active')
