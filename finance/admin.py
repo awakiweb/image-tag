d06241a30936526f68dd71715eb0005b6f802035
Manuel Perez <manuelpc2495@gmail.com>
2022-03-14 23:47:34 -0600

    add wallet

--- finance/admin.py
+++ finance/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
 
-from .models import MovementType, MovementAccount
+from .models import MovementCategory, MovementType, Wallet, MovementAccount
+
+
+@admin.register(MovementCategory)
+class MovementCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'active')
 
 
 @admin.register(MovementType)
-class MoneyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'id', 'type', 'active')
+class MovementTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'type', 'category', 'active')
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('user', 'id', 'name', 'active')
 
 
 @admin.register(MovementAccount)
-class MoneyAdmin(admin.ModelAdmin):
+class MovementAccountAdmin(admin.ModelAdmin):
     list_filter = ('movement_type',)
-    list_display = ('movement_type', 'id', 'money', 'date', 'value')
-
+    list_display = ('movement_type', 'id', 'wallet', 'money', 'date', 'value')
--- finance/models.py
+++ finance/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from money.models import Money
+from django.contrib.auth.models import User
 
 
-# Create your models here.
+class MovementCategory(models.Model):
+    name = models.CharField(max_length=150)
+    active = models.BooleanField(default=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
+    updated_at = models.DateField(auto_now=True, null=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
 class MovementType(models.Model):
     ENTRY = 'E'
     DEPARTURE = 'S'
@@ -13,7 +24,22 @@ class MovementType(models.Model):
     ]
 
     type = models.CharField(max_length=1, choices=MOVEMENT_CHOICES, default=ENTRY)
+    category = models.ForeignKey(MovementCategory, related_name='movement_types', on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
+    active = models.BooleanField(default=True)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
+    updated_at = models.DateField(auto_now=True, null=True)
+
+    def __str__(self):
+        return "{}".format(self.name)
+
+
+class Wallet(models.Model):
+    user = models.ForeignKey(User, related_name='wallets', on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=150)
+    start_value = models.FloatField(default=0)
     active = models.BooleanField(default=True)
 
     created_at = models.DateField(auto_now_add=True, null=True)
@@ -25,10 +51,12 @@ class MovementType(models.Model):
 
 class MovementAccount(models.Model):
     money = models.ForeignKey(Money, related_name='movement_accounts', on_delete=models.CASCADE)
+    wallet = models.ForeignKey(Wallet, related_name='movement_accounts', on_delete=models.CASCADE)
     movement_type = models.ForeignKey(MovementType, related_name='movement_accounts', on_delete=models.CASCADE)
 
-    value = models.FloatField()
     date = models.DateField()
+    value = models.FloatField()
+    active = models.BooleanField(default=True)
 
     created_at = models.DateField(auto_now_add=True, null=True)
     updated_at = models.DateField(auto_now=True, null=True)
